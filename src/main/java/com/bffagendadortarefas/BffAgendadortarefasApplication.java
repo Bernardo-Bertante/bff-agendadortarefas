cdcbc765cafe9821b1fce89a33144508f8e02241
Bernardo Bertante <bernardo.sctp@gmail.com>
2025-12-17 19:07:51 -0300

    inicializando projeto

---
+++ src/main/java/com/bffagendadortarefas/BffAgendadortarefasApplication.java
@@ -0,0 +1,13 @@
+package com.bffagendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffAgendadortarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffAgendadortarefasApplication.class, args);
+	}
+
+}
